1673a7b2d629eb1fcf601949cec16b26e5b53470
Mchaack <meghan.haack@gmail.com>
2018-08-03 22:04:16 -0700

    most of the things and stuff

---
+++ app/data/friend.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Meg',
+    profilePic: '',
+    scores: [
+      "3",
+      "1",
+      "4",
+      "4",
+      "5",
+      "2",
+      "5",
+      "3",
+      "4",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
